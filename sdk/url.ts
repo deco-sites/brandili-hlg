9d5c2e876e7fcbe715a96e628d18453ae88f6fa9
Caio <programmercaio98@gmail.com>
2025-07-07 23:05:13 -0300

    feat: excluindo tudo

--- sdk/url.ts
+++
@@ -1,5 +0,0 @@
-export const relative = (link?: string | undefined) => {
-  const linkUrl = link ? new URL(link, "http://localhost") : undefined;
-  const linkPath = linkUrl ? `${linkUrl.pathname}${linkUrl.search}` : undefined;
-  return linkPath;
-};
